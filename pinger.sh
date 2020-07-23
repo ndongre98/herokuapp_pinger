365cf79a02cc4f25937c84ec0d3715bc3ad825de
Namita Dongre <ndongre@andrew.cmu.edu>
2020-07-23 13:37:30 -0400

    Mac friendly shell script

--- pinger.sh
+++ pinger.sh
@@ -22,13 +22,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
-#
-# Repository https://github.com/sharadcodes/herokuapp_pinger.git
+# EDITED BY: NAMITA DONGRE
+# Repository https://github.com/ndongre98/herokuapp_pinger.git
 
 
 
-cat herokuapp_pinger/script_data/hosts_list.txt |  while read myHost
-
+export HOME=/Users/DUMMY_USER_NAME
+cat $HOME/herokuapp_pinger/script_data/hosts_list.txt |  while read myHost
 
 #	Don't modify these configs
 # 	Looping over the list of hosts in the hosts_list.txt file
@@ -42,11 +42,7 @@ do
 	echo "$d, $t, $day, $status" >> herokuapp_pinger/csv_logs/${myHost}.csv	
 	
 	
-# 	IMPORTANT :  CHANGE DUMMY_USER_NAME with your username
-	export HOME=/home/DUMMY_USER_NAME
-	
-	
 #   Dont modify these configs
 	export DISPLAY=:0.0
-	notify-send -i $HOME/herokuapp_pinger/script_data/icons/h_icon.png "$myHost" "Status: $status"
+	osascript -e 'display notification "'"$status"'" with title "'"$myHost"'"'
 done
